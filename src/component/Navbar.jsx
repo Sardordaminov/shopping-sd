37bc4db04d0341cf396e45f243e5229e743e2f1c
Sardordaminov <sd6795838@gmail.com>
2023-11-20 14:14:22 +0500

    salom

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,106 +1,160 @@
-import React, { useState } from 'react'
-import { FaShoppingCart, FaHeart, FaUser, FaTimes } from 'react-icons/fa'
-
-const showOrders = (props) => {
-    let allprice = 0;
-    props.orders.forEach(el => {
-        allprice += Number.parseFloat(el.price)
-    });
-
-    let fee = Math.floor(allprice / 100 * 5)
-
-    return (
-        <div className='order-cart-item'>
-            {props.orders.map((el) => (
-                <div className='Item' key={el.id}>
-                    <div className='imgBx'>
-                        <img src={'./img/' + el.img} alt='hello' />
-                    </div>
-                    <div className='ItemBx'>
-                        <h1>{el.title}</h1>
-                        <p>{el.price}₽</p>
-                    </div>
-                    <button onClick={() => { props.onDelete(el.id) }} className='close-box'><FaTimes className='close' /></button>
-                </div>
-            ))}
-            <div className='all-price'>
-                <p className='allprice'>Итого: {allprice}руб.</p>
-                <p className='orders-lenght'>Налог 5%: {fee}руб.</p>
-            </div>
-        </div>
-    )
-}
-const showFavorites = (props) => {
-    return (
-        <div className='favorite-cart-item'>
-            {props.favorite.map((el) => (
-                <div className='Item' key={el.id}>
-                    <div className='imgBx'>
-                        <img src={'./img/' + el.img} alt='hello' />
-                    </div>
-                    <div className='ItemBx'>
-                        <h1>{el.title}</h1>
-                        <p>{el.price}₽</p>
-                    </div>
-                    <button onClick={() => { props.onDelete1(el.id) }} className='close-box'><FaTimes className='close' /></button>
-                </div>
-            ))}
-        </div>
-    )
-}
-
-const showNothing = (name) => {
-    return (
-        <div className='nothing'>
-            <h2>{name} пустая &#128547;</h2>
-        </div>
-    )
-}
+import React, { useState } from "react";
+import { FaShoppingCart, FaHeart, FaUser, FaTimes } from "react-icons/fa";
 
 const Navbar = (props) => {
-    let [cartopen, setCartOpen] = useState(false)
-    let [favorite1, setFavorite1] = useState(false)
-    let [nav, setNav] = useState(false)
+  let [cartopen, setCartOpen] = useState(false);
+  let [favorite1, setFavorite1] = useState(false);
+  let [nav, setNav] = useState(false);
+
+
+  const showOrders = (props) => {
+    let allprice = 0;
+    props.orders.forEach((el) => {
+      allprice += Number.parseFloat(el.price);
+    });
+
+    let fee = Math.floor((allprice / 100) * 5);
 
     return (
-        <header className={`header ${cartopen ? "active" : ""} ${favorite1 ? "active1" : ""}`}>
-            <div className='logo'>
-                <img src={'./img/logo.jpg'} alt='' />
-                <div>
-                    <h4>REACT SNEAKERS</h4>
-                    <p>Магазин лучших кроссовок</p>
-                </div>
+      <div className="order-cart-item">
+        {props.orders.map((el) => (
+          <div className="Item" key={el.id}>
+            <div className="imgBx">
+              <img src={"./img/" + el.img} alt="hello" />
             </div>
-            <ul className={`nav ${nav && "active"}`}>
-                <button className='close-box btn' id='nav_button' onClick={() => setNav(nav => !nav)}><FaTimes className='close' /></button>
-                <li className='li'><FaShoppingCart onClick={() => setCartOpen(cartopen => !cartopen)} className='open btn' />{props.orders.length > 0 && <div className='orders-length'></div>}<p>1205 рубл.</p></li>
-                <li><FaHeart onClick={() => setFavorite1(favorite1 = !favorite1)} className='btn' />{props?.favorite?.length > 0 && <div className='favorite-length'></div>}</li>
-                <li><FaUser className='btn' /></li>
-            </ul>
-            <i className="fa-solid fa-bars" onClick={() => setNav(nav => !nav)}></i>
-
-            <div className='orders'>
-                <div className='orders-title'>
-                    <h2>Корзина</h2>
-                    <button className='close-box btn' onClick={() => setCartOpen(cartopen => !cartopen)}><FaTimes className='close' /></button>
-                </div>
-                <div className='order-cart'>
-                    {props?.orders?.length > 0 ? showOrders(props) : showNothing("Корзина")}
-                </div>
+            <div className="ItemBx">
+              <h1>{el.title}</h1>
+              <p>{el.price}₽</p>
             </div>
-
-
-            <div className='favorite'>
-                <div className='favorite-title'>
-                    <h2>Мои закладки</h2>
-                    <button className='close-box btn' onClick={() => setFavorite1(favorite1 => !favorite1)}><FaTimes className='close' /></button>
-                </div>
-                <div className='favorite-cart'>
-                    {props?.favorite?.length > 0 ? showFavorites(props) : showNothing("Закладок")}
-                </div>
+            <button
+              onClick={() => {
+                props.onDelete(el.id);
+              }}
+              className="close-box"
+            >
+              <FaTimes className="close" />
+            </button>
+          </div>
+        ))}
+        <div className="all-price">
+          <p className="allprice">Итого: {allprice}руб.</p>
+          <p className="orders-lenght">Налог 5%: {fee}руб.</p>
+        </div>
+      </div>
+    );
+  };
+  const showFavorites = (props) => {
+    return (
+      <div className="favorite-cart-item">
+        {props.favorite.map((el) => (
+          <div className="Item" key={el.id}>
+            <div className="imgBx">
+              <img src={"./img/" + el.img} alt="hello" />
             </div>
-        </header>
-    )
-}
+            <div className="ItemBx">
+              <h1>{el.title}</h1>
+              <p>{el.price}₽</p>
+            </div>
+            <button
+              onClick={() => {
+                props.onDelete1(el.id);
+              }}
+              className="close-box"
+            >
+              <FaTimes className="close" />
+            </button>
+          </div>
+        ))}
+      </div>
+    );
+  };
+  const showNothing = (name) => {
+    return (
+      <div className="nothing">
+        <h2>{name} пустая &#128547;</h2>
+      </div>
+    );
+  };
 
-export default Navbar
+  return (
+    <header
+      className={`header ${cartopen ? "active" : ""} ${
+        favorite1 ? "active1" : ""
+      }`}
+    >
+      <div className="logo">
+        <img src={"./img/logo.jpg"} alt="" />
+        <div>
+          <h4>REACT SNEAKERS</h4>
+          <p>Магазин лучших кроссовок</p>
+        </div>
+      </div>
+      <ul className={`nav ${nav && "active"}`}>
+        <button
+          className="close-box btn"
+          id="nav_button"
+          onClick={() => setNav((nav) => !nav)}
+        >
+          <FaTimes className="close" />
+        </button>
+        <li className="li">
+          <FaShoppingCart
+            onClick={() => setCartOpen((cartopen) => !cartopen)}
+            className="open btn"
+          />
+          {props.orders.length > 0 && <div className="orders-length"></div>}
+          <p>1205 рубл.</p>
+        </li>
+        <li>
+          <FaHeart
+            onClick={() => setFavorite1((favorite1 = !favorite1))}
+            className="btn"
+          />
+          {props?.favorite?.length > 0 && (
+            <div className="favorite-length"></div>
+          )}
+        </li>
+        <li>
+          <FaUser className="btn" />
+        </li>
+      </ul>
+      <i className="fa-solid fa-bars" onClick={() => setNav((nav) => !nav)}></i>
+
+      <div className="orders">
+        <div className="orders-title">
+          <h2>Корзина</h2>
+          <button
+            className="close-box btn"
+            onClick={() => setCartOpen((cartopen) => !cartopen)}
+          >
+            <FaTimes className="close" />
+          </button>
+        </div>
+        <div className="order-cart">
+          {props?.orders?.length > 0
+            ? showOrders(props)
+            : showNothing("Корзина")}
+        </div>
+      </div>
+
+      <div className="favorite">
+        <div className="favorite-title">
+          <h2>Мои закладки</h2>
+          <button
+            className="close-box btn"
+            onClick={() => setFavorite1((favorite1) => !favorite1)}
+          >
+            <FaTimes className="close" />
+          </button>
+        </div>
+        <div className="favorite-cart">
+          {props?.favorite?.length > 0
+            ? showFavorites(props)
+            : showNothing("Закладок")}
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Items from "../../component/Items";
 import Navbar from "../../component/Navbar";
 
 const Home = () => {
-  //   const [orders, setOrders] = useState([]);
-  //   const [favorite, setFavorite] = useState([]);
-
   const [orders, setOrders] = useState([]);
   const [favorite, setFavorite] = useState([]);
+  
   let [items, setItems] = useState([
     {
       id: 1,
@@ -130,51 +128,6 @@ const Home = () => {
       inOrder: false,
     },
   ]);
-
-
-
-
-  //   const addOrder = (item) => {
-  //     let isInArray = false;
-  //     orders.forEach((el) => {
-  //       if (el.id === item.id) {
-  //         isInArray = true;
-  //       }
-  //     });
-  //     if (!isInArray) {
-  //       setOrders((prevOrders) => [...prevOrders, item]);
-
-  //       setItems((prevItem) =>
-  //         prevItem.map((el) =>
-  //           el.id === item.id ? { ...el, inOrder: true } : el
-  //         )
-  //       );
-  //     } else {
-  //       alert("Уже в корзине");
-  //     }
-  //   };
-
-  //   const addFavorite = (item) => {
-  //     let isInArray = false;
-
-  //     favorite.forEach((el) => {
-  //       if (el.id === item.id) {
-  //         isInArray = true;
-  //       }
-  //     });
-
-  //     if (!isInArray) {
-  //       setFavorite((prevFavorite) => [...prevFavorite, item]);
-
-  //       setItems((prevItems) =>
-  //         prevItems.map((el) =>
-  //           el.id === item.id ? { ...el, inFavorite: true } : el
-  //         )
-  //       );
-  //     } else {
-  //       alert("Уже в избранном");
-  //     }
-  //   };
   const addOrder = (item) => {
     let isInArray = false;
     orders.forEach((el) => {
@@ -221,23 +174,6 @@ const Home = () => {
       alert("Уже в избранном");
     }
   };
-  //   const deleteOrder = (id) => {
-  //     setOrders((prevOrder) =>
-  //       prevOrder.filter((el) => el.id !== id && el.inOrder === false)
-  //     );
-  //     setItems((prevItem) =>
-  //       prevItem.map((el) => (el.id === id ? { ...el, inOrder: false } : el))
-  //     );
-  //   };
-
-  //   const deleteFavorite = (id) => {
-  //     setFavorite((prevFavorite) => prevFavorite.filter((el) => el.id !== id));
-  //     setItems((prevItems) =>
-  //       prevItems.map((el) => (el.id === id ? { ...el, inFavorite: false } : el))
-  //     );
-  //   };
-
-
 
   const deleteOrder = (id) => {
     setOrders((prevOrder) =>
@@ -258,24 +194,6 @@ const Home = () => {
     const updatedFavorites = favorite.filter((el) => el.id !== id);
     localStorage.setItem("favorites", JSON.stringify(updatedFavorites));
   };
-
-  // The useEffect hook is used to perform side effects in a functional component.
-// It takes a callback function as its first argument.
-// The callback function will be executed after the component renders for the first time and whenever any dependencies change.
-// In this case, an empty dependency array ([]) is provided, indicating that the effect should run only once after the initial render.
-
-useEffect(() => {
-  // localStorage.getItem('favorites') retrieves the value associated with the key 'favorites' from the browser's local storage.
-  // The value is initially stored as a string.
-
-  // JSON.parse() is a JavaScript function that parses a JSON string, converting it into a JavaScript object.
-  // Here, it is used to convert the string retrieved from local storage into a JavaScript object.
-  const favoritesFromLocalStorage = JSON.parse(localStorage.getItem('favorites'));
-
-  // setFavorite() is a state-setting function. It is used to update the state variable 'favorite' with the value obtained from localStorage.
-  // This sets the initial state of 'favorite' when the component first renders.
-  setFavorite(favoritesFromLocalStorage);
-}, []); // The empty dependency array ensures that this effect runs only once after the initial render.
 
   return (
     <>
